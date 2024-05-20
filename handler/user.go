61880dfca8db6b96b391b6d26fd314e76432ebbf
Davidanat <david.if2012@gmail.com>
2024-05-20 11:11:09 +0700

    login function

--- handler/user.go
+++ handler/user.go
@@ -38,9 +38,41 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	// token, err :=h.jwtService.GenerateToken()
 
-	formatter := user.FormatterUser(newUser, "365238rfhwoiqfhwety43yt03ut0ewwfw")
+	formatter := user.FormatUser(newUser, "365238rfhwoiqfhwety43yt03ut0ewwfw")
 
 	response := helper.APIResponse("Account has been registered", http.StatusOK, "Success", formatter)
 
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	userLogin, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(userLogin, "tokentokentokentokentoken")
+
+	response := helper.APIResponse("Successfuly Login", http.StatusOK, "Success", formatter)
+	c.JSON(http.StatusOK, response)
+}
+
+func CheckEmail(c *gin.Context) {
+
 }
